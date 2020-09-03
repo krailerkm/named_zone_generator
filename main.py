56f8c99d12f2fd5e7dd1d824836099bc50e6e97a
KRAILERKM <krailerk_man@hotmail.com>
2020-09-03 15:19:49 +0700

    09-03-2020 Create blockfile

---
+++ main.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# 9 September 2020 Codeing by Krailerk M.
+# This script for convert txt list of url to named new zone for block in pdns
+#                                           
+#  _____         _ _         _      _____   
+# |  |  |___ ___|_| |___ ___| |_   |     |  
+# |    -|  _| .'| | | -_|  _| '_|  | | | |_ 
+# |__|__|_| |__,|_|_|___|_| |_,_|  |_|_|_|_|
+#                                           
+#
+import datetime
+
+def main():
+    ## Run date time
+    today = datetime.date.today()
+    #print(today.strftime("%d %b %Y"))
+    ## Open file for read all url from text
+    fileOP = open("./input/urllist.txt", "r")
+    ## Readline from file to parameter
+    beforeCUT = fileOP.readlines()
+    ## Close file for read all url from text
+    fileOP.close()
+    ## Parameter for store string
+    outCUT = str()
+    ## Loop for split only url cut off part and record
+    for n in beforeCUT:
+        ## Cut off http://
+        if n[:7] == "http://":
+            ## CUT http://
+            tmp = n[7:]
+            #print(tmp.split('/')[0])
+            ## Store to string
+            outCUT += tmp.split('/')[0]
+        ## Cut off https://
+        elif n[:8] == "https://":
+            ## CUT https://
+            tmp = n[8:]
+            #print(tmp.split('/')[0])
+            ## Store to string
+            outCUT += tmp.split('/')[0]
+        ## Cut off not domain and part
+        #elif n[:2] == "\n":
+            ## Pass any things
+        #    pass
+        ## Any domain and part
+        else:
+            ## Any word sotore
+            tmp = n
+            #print(tmp.split('/')[0])
+            ## Store to string
+            outCUT += tmp.split('/')[0]
+    #print(outCUT)
+    ## Find unique url from many url list
+    unique_words = set(outCUT.split())
+    #print(unique_words)
+    ## Name of blocker
+    nameBlock = "Krailerk M."
+    ## Name of requester
+    nameRequest = "Sopis J."
+    ## Number of case from Court's order
+    noCase = "Black Case No. 15/2013, Red Case No. 171/2013"
+    ## Generate comment of line
+    tmpstr1 = "//// " + nameBlock + " //// Request by " + nameRequest + " //// According to the Court's order, " + noCase + " //// " + str(today.strftime("%d %b %Y")) + "\n"
+    ## Str parameter for store data record zone
+    tmpstr = str()
+    ## Loop for generate zone file
+    for tmpUniqueURL in unique_words:
+        ## Create zone record and link to zone file
+        tmpstr2 = "zone \"" + tmpUniqueURL + "\" IN {\n        type master;\n        file \"data/db.sexsiam.com\";\n        allow-update { none; };\n};\n\n"
+        ## Put all data to tmp string
+        tmpstr += tmpstr1 + tmpstr2
+    ## Show output
+    print(tmpstr)
+    ## Open output file
+    fileOut = open("./output/outzone.txt", "w")
+    ## Write Export output to file
+    fileOut.write(tmpstr)
+    ## Close file output
+    fileOut.close
+
+## Run main function
+if __name__ == "__main__":
+    ## Call main
+    main()
