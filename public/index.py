dd9fe16fc9cbd4210ecb779f612fba12523ea0ad
KRAILERKM <krailerk_man@hotmail.com>
2020-10-12 21:11:11 +0700

    10-12-2020-2110 Tried to create web cgi

---
+++ public/index.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# 9 September 2020 Codeing by Krailerk M.
+# This script for convert txt list of url to named new zone for block in pdns
+#                                           
+#  _____         _ _         _      _____   
+# |  |  |___ ___|_| |___ ___| |_   |     |  
+# |    -|  _| .'| | | -_|  _| '_|  | | | |_ 
+# |__|__|_| |__,|_|_|___|_| |_,_|  |_|_|_|_|
+#                                           
+#
+print("Content-type:text/html\r\n\r\n")
+print('<html>')
+print('<head>')
+print('<title>Hello World - First CGI Program</title>')
+print('</head>')
+print('<body>')
+print('<h2>Hello World! This is my first CGI program</h2>')
+print('</body>')
+print('</html>')
