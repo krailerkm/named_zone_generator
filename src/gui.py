657e2e880b2156306d9c4f6038cd1e1bffe97aa3
KRAILERKM <krailerk_man@hotmail.com>
2020-11-01 22:35:13 +0700

    11-1-2020-1035 Learning

--- src/gui.py
+++ src/gui.py
@@ -1,20 +1,45 @@
-import tkinter as tk
+#!/usr/bin/python
+#import tkinter as tk
 
-def set_message():
-    text = text_input.get()
-    title_label.configure(text=text)
+#def set_message():
+#    text = text_input.get()
+#    title_label.configure(text=text)
 
-windows = tk.Tk()
-windows.title('JustPython')
-windows.minsize(width=400, height=400)
+#windows = tk.Tk()
+#windows.title('JustPython')
+#windows.minsize(width=400, height=400)
 
-title_label = tk.Label(master=windows, text='Hello world')
-title_label.pack()
+#title_label = tk.Label(master=windows, text='Hello world')
+#title_label.pack()
 
-text_input = tk.Entry(master=windows)
-text_input.pack()
+#text_input = tk.Entry(master=windows)
+#text_input.pack()
 
-ok_button = tk.Button(master=windows, text='Ok', command=set_message)
-ok_button.pack()
+#ok_button = tk.Button(master=windows, text='Ok', command=set_message)
+#ok_button.pack()
 
-windows.mainloop()
+#windows.mainloop()
+
+from tkinter import tix as tx
+
+root=tx.Tk()
+root.geometry("800x600+0+10")
+
+swr=tx.ScrolledWindow(root)
+swr.pack(fill=tx.BOTH, expand=1)
+
+nb=tx.NoteBook(swr.window)
+nb.pack(fill=tx.BOTH, expand=1)
+
+for i in range(1,3):
+	nb.add("tab"+str(i),label="Tab "+str(i))
+
+for k in range(1,39):
+	l=tx.Label(nb.tab1,text="label "+str(k))
+	l.pack()
+
+for k in range(1,29):
+	l=tx.Label(nb.tab2,text="label "+str(k))
+	l.pack(side=tx.LEFT)
+
+root.mainloop()
