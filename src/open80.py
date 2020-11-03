18547a6fd68243e492bb23f339f370e70c0e4a35
KRAILERKM <krailerk_man@hotmail.com>
2020-11-03 16:37:05 +0700

    11-03-2020-1636 Create GUI and test

--- src/open80.py
+++ src/open80.py
@@ -1,3 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# 9 September 2020 Codeing by Krailerk M.
+# This script for convert txt list of url to named new zone for block in pdns
+# NCG v1.0a - (Named configuration generator version 1.0a) 
+#  _____         _ _         _      _____   
+# |  |  |___ ___|_| |___ ___| |_   |     |  
+# |    -|  _| .'| | | -_|  _| '_|  | | | |_ 
+# |__|__|_| |__,|_|_|___|_| |_,_|  |_|_|_|_|
+#                                           
+#
 import socket
 
 mysock =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- src/urllibuse.py
+++ src/urllibuse.py
@@ -1,3 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# 9 September 2020 Codeing by Krailerk M.
+# This script for convert txt list of url to named new zone for block in pdns
+# NCG v1.0a - (Named configuration generator version 1.0a) 
+#  _____         _ _         _      _____   
+# |  |  |___ ___|_| |___ ___| |_   |     |  
+# |    -|  _| .'| | | -_|  _| '_|  | | | |_ 
+# |__|__|_| |__,|_|_|___|_| |_,_|  |_|_|_|_|
+#                                           
+#
 #Used to make requests
 import urllib.request
 import ssl
